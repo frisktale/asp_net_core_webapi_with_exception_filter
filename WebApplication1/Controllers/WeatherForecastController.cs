3e704ce0ffcf6da4777936297e4923f3a01f9301
frisk <frisktale@live.com>
2021-11-19 21:16:44 +0800

    init project

---
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Filter;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    //[TypeFilter(typeof(ExceptionFilter))]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet(Name = "ExceptionTest")]
+        public IEnumerable<WeatherForecast> ExceptionTest()
+        {
+            throw new Exception("“Ï≥£≤‚ ‘");
+        }
+    }
+}
---
+++ WebApplication1/Program.cs
@@ -0,0 +1,29 @@
+using WebApplication1.Filter;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers(
+    opt=>opt.Filters.Add(typeof(ExceptionFilter))
+);
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
